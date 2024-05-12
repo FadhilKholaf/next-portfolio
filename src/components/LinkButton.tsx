8f6f69ccd0e42a8913f9f9ac7fc15e462a406428
FadhilKholaf <fadhilgd123@gmail.com>
2024-05-12 10:40:02 +0700

    feat: Add link button Components

---
+++ src/components/LinkButton.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+interface LinkButtonProps {
+  className?: string;
+  dataAos?: string;
+  dataAosDelay?: number;
+  href: string;
+  title: string;
+}
+
+export default function LinkButton({
+  className,
+  href,
+  title,
+  dataAos,
+  dataAosDelay,
+}: LinkButtonProps) {
+  return (
+    <Link
+      data-aos={dataAos}
+      data-aos-delay={dataAosDelay}
+      href={href}
+      className={`flex w-fit items-center p-2  text-sm transition duration-500 ease-in-out hover:-translate-y-2 lg:text-lg ${className}`}
+    >
+      {title}
+    </Link>
+  );
+}
