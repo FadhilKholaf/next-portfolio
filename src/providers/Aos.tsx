c05235eb87d4b64ea1a639e19f9bd2b0ca709b30
FadhilKholaf <fadhilgd123@gmail.com>
2024-05-12 10:39:19 +0700

    feat: initial AOS packange

---
+++ src/providers/Aos.tsx
@@ -0,0 +1,18 @@
+"use client";
+import AOS from "aos";
+
+import "aos/dist/aos.css";
+import React, { Fragment, useEffect } from "react";
+
+const AOSProvider = ({ children }: { children: React.ReactNode }) => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      anchorPlacement: "top-top",
+    });
+  }, []);
+
+  return <Fragment>{children}</Fragment>;
+};
+
+export default AOSProvider;
