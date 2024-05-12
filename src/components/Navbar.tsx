92506e52fbd127f437447404870b03a18becb0c1
FadhilKholaf <fadhilgd123@gmail.com>
2024-05-12 10:40:19 +0700

    feat: Add navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+import LinkButton from "./LinkButton";
+
+export default function Navbar() {
+  const NavItems: { href: string; title: string }[] = [
+    { href: "#home", title: "Home" },
+    { href: "#about", title: "About" },
+    { href: "#projects", title: "Projects" },
+    { href: "#contact", title: "Contact" },
+  ];
+  return (
+    <nav
+      data-aos="fade-down"
+      data-aos-anchor-placement="top-bottom"
+      className="fixed top-[91%] z-20 flex w-full justify-center p-4 md:top-[0%]"
+    >
+      <div className="flex w-full justify-between rounded-full border-2 border-white bg-gradient-to-r from-[#FFD49C]/5 to-[#7A87FB]/5 px-2 py-1 backdrop-blur md:w-1/2">
+        {NavItems &&
+          NavItems.map((item, index: number) => (
+            <LinkButton
+              key={index}
+              href={item.href}
+              title={item.title}
+              className="text-white"
+            />
+          ))}
+      </div>
+    </nav>
+  );
+}
